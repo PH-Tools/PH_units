3c8b1ac4f118dc456f2be412a3dfc650fe9872aa
PH Tools <phtools@bldgtyp.com>
2024-06-25 17:43:41 -0400

    fix(areas): Add more area types

--- ph_units/unit_types/area.py
+++ ph_units/unit_types/area.py
@@ -9,7 +9,28 @@ class MeterSquare(Base_UnitType):
 
     __symbol__ = "M2"
     __aliases__ = ["SM", "SQM", "SQ.M", "SQ-M", "SQ-METER", "SQ-METERS", "M²"]
-    __factors__ = {"M2": "{}*1", "FT2": "{}*10.76391042"}
+    __factors__ = {
+        "M2": "{}*1",
+        "CM2": "{}*10_000",
+        "MM2": "{}*1_000_000",
+        "FT2": "{}*10.76391042",
+    }
+
+
+class CentimeterSquare(Base_UnitType):
+    """Centimeter Square"""
+
+    __symbol__ = "CM2"
+    __aliases__ = ["SQCM", "SQ.CM", "SQ-CM", "SQ-CENTIMETER", "SQ-CENTIMETERS", "CM²"]
+    __factors__ = {"MM2": "{}*100", "CM2": "{}*1", "M2": "{}*0.0001", "FT2": "{}*1"}
+
+
+class MillimeterSquare(Base_UnitType):
+    """Millimeter Square"""
+
+    __symbol__ = "MM2"
+    __aliases__ = ["SQMM", "SQ.MM", "SQ-MM", "SQ-MILLIMETER", "SQ-MILLIMETERS", "MM²"]
+    __factors__ = {"MM2": "{}*1", "CM2": "{}*0.01", "M2": "{}*0.000001", "FT2": "{}*1"}
 
 
 class FootSquare(Base_UnitType):
--- tests/test_areas.py
+++ tests/test_areas.py
@@ -8,8 +8,26 @@ from ph_units.converter import convert
 
 def test_meter_squared():
     assert convert(1, "M2", "M2") == 1
+    assert convert(1, "M²", "M2") == 1
+    assert convert(1, "M2", "CM2") == 10_000
+    assert convert(1, "M2", "MM2") == 1_000_000
+    assert convert(00.0553, "M2", "MM2") == 55_300
     assert convert(1, "M2", "FT2") == pytest.approx(10.76391042)
-    assert convert(1, "M²", "FT2") == pytest.approx(10.76391042)
+
+
+def test_centimeter_squared():
+    assert convert(1, "CM2", "CM2") == 1
+    assert convert(1, "CM²", "CM2") == 1
+    assert convert(1, "CM2", "M2") == 0.0001
+    assert convert(1, "CM2", "MM2") == 100
+
+
+def test_millimeter_squared():
+    assert convert(1, "MM2", "MM2") == 1
+    assert convert(1, "MM²", "MM2") == 1
+    assert convert(1, "MM2", "M2") == 0.000001
+    assert convert(34_565_678, "MM2", "M2") == 34.565678
+    assert convert(1, "MM2", "CM2") == 0.01
 
 
 def test_foot_squared():
