e978c2ef7c2462af238a6175058ee29a37c0ccc7
PH Tools <phtools@bldgtyp.com>
2025-01-28 10:23:48 -0500

    fix(setup): Update to PEP-625 compliant package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PH-units",
+    name="ph_units",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     long_description=long_description,
